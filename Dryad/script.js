041093e79629f02ec9b8dc1c2fcd6cb654d3ec02
Danissa22 <32400429+Danissa22@users.noreply.github.com>
2017-11-10 17:53:41 -0500

    Eureka!
    Eureka! I got it!

---
+++ Dryad/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+        $(".header").hide(0).delay(500).fadeIn(3000)
+
+
+
+        document.getElementById(".DryadBackground").onload = function()
+        {myFunction(){
+              x.style.WebkitAnimation = "mymove 4s 2";
+
+        }
+
+
+
+
+}
